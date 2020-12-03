203833f67f070c869a4423b94c035553e24d9b10
Colm McSharry <colmmcsharry@hotmail.com>
2020-12-02 16:19:26 -0800

    added map back in

--- src/components/map.js
+++ src/components/map.js
@@ -5,7 +5,7 @@ function Mymap() {
   return (
     <div>
     
-    
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.33097122297!2d-123.01817518434723!3d49.27012547958134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5486772cc008827f%3A0x7cb02acff7db5db!2sCrezo%20Construction%20Inc.!5e0!3m2!1sen!2sca!4v1606950931646!5m2!1sen!2sca" width="350" height="350" loading="lazy" frameBorder="0" style={{marginTop:'40px'}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
     </div>
   );
 }
